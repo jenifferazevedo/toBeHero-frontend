ce440179b346c82f1e3ce0e3adc6f111e9f64759
Jeniffer Q. F. R. de Azevedo <jeniqf@gmail.com>
2020-03-24 22:41:26 +0000

    //Back-end, controllers, routes and cors
    /*The Back-end has been improved. Controllers were created for the creation, listing and delete functions. Routes for ONG authentication, private listing for the ONG, creation of incidents and delete. Public routes for incident research. */

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,28 +1,21 @@
 const express = require('express');
-const crypto = require('crypto');
-const connection = require('./database/connection');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+
 
 const routes = express.Router();
 
-routes.get('/ongs', async (req, res) => {
-  const ongs = await connection('ongs').select('*');
-  return res.json(ongs);
-})
+routes.post('/session', SessionController.create);
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
-routes.post('/ongs', async (req, res) => {
-  const { name, email, whatsapp, city, uf } = req.body;
-  const id = crypto.randomBytes(4).toString('HEX');
-  
-  await connection('ongs').insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf,
-  })
-  
-  return res.json({ id });
-})
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
