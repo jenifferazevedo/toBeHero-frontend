de498a4f94ce8276bdd03f71fb1bc533d13b2934
Jeniffer Q. F. R. de Azevedo <jeniqf@gmail.com>
2020-03-24 16:11:49 +0000

    //Database, connection and routes
    175/5000
    The database was configured, the migrations were created and the tables were structured. After connecting the back-end to the database and starting some get / post methods for testing.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,28 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.post('/users', (req, res) => {
-  const body = req.body;
-  console.log(body)
+routes.get('/ongs', async (req, res) => {
+  const ongs = await connection('ongs').select('*');
+  return res.json(ongs);
+})
 
-  return res.json({
-    evento: 'Semana OmniStack 11.0',
-    aluno: 'Jeniffer Azevedo',
-    saudacao: 'Vamos codar!'
-  });
+routes.post('/ongs', async (req, res) => {
+  const { name, email, whatsapp, city, uf } = req.body;
+  const id = crypto.randomBytes(4).toString('HEX');
+  
+  await connection('ongs').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf,
+  })
+  
+  return res.json({ id });
 })
 
 module.exports = routes;
