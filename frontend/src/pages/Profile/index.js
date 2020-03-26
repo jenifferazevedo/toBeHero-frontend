97795bbccb57db2094e355866aa20ba56780ed0e
Jeniffer Q. F. R. de Azevedo <jeniqf@gmail.com>
2020-03-26 14:59:23 +0000

    //Front-end: Profile page
    /*Logout was developed, cleaning up the 'localStorage'*/

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState ,useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 import api from '../../services/api';
 import './styles.css';
@@ -7,6 +7,7 @@ import logoImg from '../../assets/logo.svg';
 
 export default function Profile() {
   const [incidents, setIncidents] = useState([]);
+  const history = useHistory();
   const ongId = localStorage.getItem('ongId');
   const ongName = localStorage.getItem('ongName');
   
@@ -33,6 +34,11 @@ export default function Profile() {
     }
   }
 
+  function handleLogout() {
+    localStorage.clear();
+    history.push('/');
+  }
+
   return (
   <div className="profile-container">
     <header>
@@ -40,7 +46,7 @@ export default function Profile() {
       <span>Bem vinda, {ongName}</span>
 
       <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-      <button type="button">
+      <button type="button" onClick={handleLogout}>
         <FiPower size={18} color="#E02041" />
       </button>
     </header>
